a03f16b3491c62969d34bc66190d157bc426dfba
NinaSleephead <kx1214@gmail.com>
2016-08-23 21:53:38 -0400

    Embedding-Data-jQuery-Practice
    Practice for embedding data with jQuery

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*Nina Kong*/
+$(document).ready(function () {
+    var windowHeight = $( window ).height();
+    var con = $(".Content");
+    con.parentsUntil($("body")).css("height", windowHeight);
+
+    $('#display').click(function (event) {
+        alert(JSON.stringify($(".Embedding").data(), null, ""));
+    });
+    $('#save').click(function (event) {
+        $(".Embedding").data("key2", 2020);
+    });
+    $('#delete').click(function (event) {
+        $(".Embedding").removeData();
+    });
+});
